d520a0fd6a896ab3c5bf37649b6aac7b8502115f
IbragimovaRoksana <roksalana91@mail.ru>
2020-12-20 18:58:09 +0500

    Code-reformat code

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -1,37 +1,35 @@
 package ru.netology.radio;
 
 
-
-
 public class Radio {
     int currentStation;
     int volume;
 
     public void setCurrentStation(int Station) {
-        currentStation=Station;
+        currentStation = Station;
     }
 
-    public void upCurrentStation(){
+    public void upCurrentStation() {
         currentStation++;
-        if (currentStation>9)
-            currentStation=0;
+        if (currentStation > 9)
+            currentStation = 0;
     }
 
-    public void downCurrentStation(){
+    public void downCurrentStation() {
         currentStation--;
-        if (currentStation<0)
-            currentStation=9;
+        if (currentStation < 0)
+            currentStation = 9;
     }
 
-    public void upVolume(){
+    public void upVolume() {
         volume++;
-        if (volume>10)
-            volume=10;
+        if (volume > 10)
+            volume = 10;
     }
 
-    public void downVolume(){
+    public void downVolume() {
         volume--;
-        if (volume<0)
-            volume=0;
+        if (volume < 0)
+            volume = 0;
     }
 }
